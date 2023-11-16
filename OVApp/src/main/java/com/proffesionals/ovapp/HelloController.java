97fea04ae40311addde50a333d993e85758856b7
Lars <egmondlars@gmail.com>
2023-11-16 09:32:58 +0100

    tekst aanpassing

--- OVApp/src/main/java/com/proffesionals/ovapp/HelloController.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Trek een bak als je dit leest");
     }
 }
