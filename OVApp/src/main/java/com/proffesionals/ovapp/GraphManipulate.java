6f0c21b61d5f9204ddc28241c7b6abbd92bfa677
lucas Winckers <150806201+LPSWinckers@users.noreply.github.com>
2023-12-01 09:58:06 +0100

    finishing touches backend

--- OVApp/src/main/java/com/proffesionals/ovapp/GraphManipulate.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/GraphManipulate.java
@@ -1,18 +1,22 @@
 package com.proffesionals.ovapp;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 public class GraphManipulate { // class for manipulating the graph
 
-    public List<Edge> getRoute(Point start, Point end, Graph graph){ // returns a list of edges that form the route from start to end
+    public Map<Edge, LocalTime> getRoute(Point start, Point end, Graph graph, LocalTime starttime){ // returns a list of edges that form the route from start to end
+        Map<Edge,LocalTime> returnMap = new LinkedHashMap();
         List<Edge> route = new ArrayList<>();
-        Integer startIndex = getPointIndex(start, graph);
+       Integer startIndex = getPointIndex(start, graph);
         Integer endIndex = getPointIndex(end, graph);
         if(startIndex == null || endIndex == null){ // if start or end point is not in the graph
             return null;
         } else if (startIndex > endIndex){ // if start point is higher than end point
-            for (int i = startIndex; i > endIndex; i--) {
+            for (int i = startIndex - 1; i > endIndex; i--) {
                 route.add(graph.getEdgesDown().get(i)); // add edges to route
             }
         } else if (startIndex < endIndex){ // if start point is lower than end point
@@ -20,7 +24,15 @@ public class GraphManipulate { // class for manipulating the graph
                 route.add(graph.getEdgesUp().get(i)); // add edges to route
             }
         }
-        return route; // return route
+        for (Edge edge : route) {
+            returnMap.put(edge, getTime(starttime, edge)); 
+            starttime = getTime(starttime, edge); // set starttime to the time of arrival at the end point of the edge
+        }
+        return returnMap; // return route
+    }
+
+    private LocalTime getTime(LocalTime time, Edge edge){ // returns the time of arrival at the end point of an edge
+        return time.plusMinutes(edge.getDistance());
     }
 
     private Integer getPointIndex(Point point, Graph graph){ // returns the index of a point in the graph
--- OVApp/src/main/java/com/proffesionals/ovapp/HelloApplication.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/HelloApplication.java
@@ -6,9 +6,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class HelloApplication extends Application {
     @Override
@@ -21,35 +22,37 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        // intializeGraph();
+        intializeGraph();
         launch();
     }
 
-    // public static void intializeGraph(){ // tet intializeGraph method
-    //     ArrayList<Point> points = new ArrayList<>();
-    //     ArrayList<Edge> edgesUp = new ArrayList<>();
-    //     ArrayList<Edge> edgesDown = new ArrayList<>();
+    public static void intializeGraph(){ // tet intializeGraph method
+        GraphManipulate graphManipulate = new GraphManipulate();
+        ArrayList<Point> points = new ArrayList<>();
+        ArrayList<Edge> edgesUp = new ArrayList<>();
+        ArrayList<Edge> edgesDown = new ArrayList<>();
 
-    //     points.add(new Point("Utrecht Centraal"));
-    //     points.add(new Point("Amsterdam Centraal"));
-    //     points.add(new Point("Rotterdam Centraal"));
-    //     points.add(new Point("Den Haag Centraal"));
-    //     points.add(new Point("Eindhoven Centraal"));
-    //     points.add(new Point("Maastricht Centraal"));
+        points.add(new Point("Utrecht Centraal"));
+        points.add(new Point("Amsterdam Centraal"));
+        points.add(new Point("Rotterdam Centraal"));
+        points.add(new Point("Den Haag Centraal"));
+        points.add(new Point("Eindhoven Centraal"));
+        points.add(new Point("Maastricht Centraal"));
 
-    //     for (int i = 0; i < points.size() - 1; i++) {
-    //         edgesUp.add(new Edge(points.get(i), points.get(i+1), 10));
-    //     }
-    //     for (int i = points.size() - 1; i > 0; i--) {
-    //         edgesDown.add(new Edge(points.get(i), points.get(i-1), 10));
-    //     }
+        for (int i = 0; i < points.size() - 1; i++) {
+            edgesUp.add(new Edge(points.get(i), points.get(i+1), 15));
+        }
+        for (int i = 0; i < points.size() - 1; i++) {
+            edgesDown.add(new Edge(points.get(i+1), points.get(i), 15));
+        }
 
-    //     Graph graph = new Graph(edgesUp, edgesDown, points); // create new graph
+        Graph graph = new Graph(edgesUp, edgesDown, points); // create new graph
 
-    //     List<Edge> route = new GraphManipulate().getRoute(graph.getPointByName("Utrecht Centraal"), graph.getPointByName("Maastricht Centraal"), graph); // get route from Utrecht to Maastricht
-    //     for (Edge edge : route) {
-    //         System.out.format("van: %s / naar: %s afstand: %s km \n", edge.getPoint1().getName(), edge.getPoint2().getName(), edge.getDistance()); // print route
-    //     }
-    // }
+        Map<Edge, LocalTime> route = graphManipulate.getRoute(graph.getPointByName("Amsterdam Centraal"), graph.getPointByName("Maastricht Centraal"), graph, LocalTime.now()); // get route from Utrecht to Maastricht
+        for (Map.Entry<Edge, LocalTime> entry : route.entrySet()) {
+            String formattedTime = entry.getValue().format(DateTimeFormatter.ofPattern("HH:mm")); // format time
+            System.out.format("van: %s / naar: %s om: %s\n", entry.getKey().getPoint1().getName(), entry.getKey().getPoint2().getName(), formattedTime); // print route
+        }
+    }
 
 }
