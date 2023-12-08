8842595316b067c4960a7bb0461194596f7bc7dc
lucas Winckers <150806201+LPSWinckers@users.noreply.github.com>
2023-12-08 11:28:15 +0100

    i dit the map

--- OVApp/src/main/java/com/proffesionals/ovapp/HelloApplication.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/HelloApplication.java
@@ -32,22 +32,27 @@ public class HelloApplication extends Application {
         ArrayList<Edge> edgesUp = new ArrayList<>();
         ArrayList<Edge> edgesDown = new ArrayList<>();
 
+        points.add(new Point("Den Helder Centraal"));
         points.add(new Point("Utrecht Centraal"));
         points.add(new Point("Amsterdam Centraal"));
-        points.add(new Point("Rotterdam Centraal"));
-        points.add(new Point("Den Haag Centraal"));
+        points.add(new Point("s-Hertogenbosch Centraal"));
         points.add(new Point("Eindhoven Centraal"));
+        points.add(new Point("Roermond Centraal"));
         points.add(new Point("Maastricht Centraal"));
 
-        for (int i = 0; i < points.size() - 1; i++) {
-            edgesUp.add(new Edge(points.get(i), points.get(i+1), 15));
-        }
-        for (int i = 0; i < points.size() - 1; i++) {
-            edgesDown.add(new Edge(points.get(i+1), points.get(i), 15));
-        }
+        addEdges(edgesUp, edgesDown, points.get(0), points.get(1), 65);
+        addEdges(edgesUp, edgesDown, points.get(1), points.get(2), 34);
+        addEdges(edgesUp, edgesDown, points.get(2), points.get(3), 46);
+        addEdges(edgesUp, edgesDown, points.get(3), points.get(4), 31);
+        addEdges(edgesUp, edgesDown, points.get(4), points.get(5), 45);
+        addEdges(edgesUp, edgesDown, points.get(5), points.get(6), 43);
 
         return new Graph(edgesUp, edgesDown, points); // create new graph
 
     }
 
+    public static void addEdges(ArrayList<Edge> edgesUp, ArrayList<Edge> edgesDown, Point point1, Point point2, Integer distance){ // add edges to graph
+        edgesUp.add(new Edge(point1, point2, distance));
+        edgesDown.add(new Edge(point2, point1, distance));
+    }
 }
