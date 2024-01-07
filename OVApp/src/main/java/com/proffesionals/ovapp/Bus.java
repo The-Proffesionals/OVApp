9286dc2d091f6911e2ede8e486da792e1a326791
lucas Winckers <150806201+LPSWinckers@users.noreply.github.com>
2024-01-07 17:03:57 +0100

    Update timeVariable in Bus class

--- OVApp/src/main/java/com/proffesionals/ovapp/Bus.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/Bus.java
@@ -3,7 +3,7 @@ package com.proffesionals.ovapp;
 public class Bus extends Edge{
     float priceConstant = 1.25f;
     float priceVariable = 0.96f;
-    float timeVariable = 0.15f;
+    float timeVariable = 0.95f;
     
     Bus(Edge edge){
         super(edge.getPoint1(), edge.getPoint2(), edge.getDistance());
