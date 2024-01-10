1968be87ac207375ebf8e86969fe163398cc036c
NickCode0000 <Veltheernick@gmail.com>
2024-01-10 14:58:17 +0100

    Knoppen labels Endcontoller

--- OVApp/src/main/java/com/proffesionals/ovapp/EndController.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/EndController.java
@@ -3,6 +3,7 @@ package com.proffesionals.ovapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -23,6 +24,50 @@ public class EndController {
     }
 
 
+    //lables:
+    @FXML
+    protected void date() {
+    }
+    @FXML
+    protected void duration(){
+
+    }
+    @FXML
+    protected void departureTime(){
+
+    }
+    @FXML
+    protected void arrivalTime(){
+
+    }
+    @FXML
+    protected void spoor(){
+
+    }
+    @FXML
+    protected void spoor2(){
+
+    }
+    @FXML
+    protected void travelInformation(){
+
+    }
+    @FXML
+    protected void intermediateStops(){
+
+    }
+    @FXML
+    protected void money(){
+
+    }
+    @FXML
+    protected void departureDestination(){
+
+    }
+    @FXML
+    protected void arrivalDestination(){
+
+    }
 
 
 }
