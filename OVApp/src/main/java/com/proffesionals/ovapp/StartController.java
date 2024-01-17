cd5ecf57e68751cfe0a85744c98c77fcdc17542e
lucas Winckers <150806201+LPSWinckers@users.noreply.github.com>
2024-01-17 13:56:41 +0100

    Remove unused buttons in EndController and FavoriteController

--- OVApp/src/main/java/com/proffesionals/ovapp/StartController.java
+++ OVApp/src/main/java/com/proffesionals/ovapp/StartController.java
@@ -3,6 +3,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.io.IOException;
 import java.util.Locale;
 import static com.proffesionals.ovapp.LanguageManager.setLanguage;
